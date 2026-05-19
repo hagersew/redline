3fcb6dc0179aa3019aee59451ae5b4cfd2ec379d
Abel Adane <contacttoabel@gmail.com>
2026-05-19 13:57:16 +0300

    Update default color mode to light in theme configuration and index script. Adjust color scheme handling for improved user experience.

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, type StyleFunctionProps, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "dark",
+  initialColorMode: "light",
   useSystemColorMode: false,
 };
 
